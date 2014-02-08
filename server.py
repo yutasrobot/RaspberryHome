0faccddb1a0a4e0ba1143640fcef04514ff64d9d
muhammed mustafa akdeniz <makdeniz@pals.com.tr>
2014-02-08 20:56:53 +0200

    pin sayisi + sonsuz while iptal

--- server.py
+++ server.py
@@ -3,12 +3,15 @@
 import RPi.GPIO as GPIO 	#raspberry pi'nin pinlerini kontrol kutuphanesini ekle
 import time
 
+led1=18
+led2=23
+
 GPIO.setmode(GPIO.BCM) 	#pin numaralarini boarddaki siralamaya gore ayarla
 GPIO.cleanup()			#onceden kalmis olan pin ayarlarini temizle
 GPIO.setup(18,GPIO.OUT)		#led 1in bagli olacagi pini cikis olarak ayarla
 
-while 1:                                
-	GPIO.output(pin,GPIO.HIGH)              #ledi yak
+while range(1,5):                                
+	GPIO.output(led1,GPIO.HIGH)              #ledi yak
 	time.sleep(1)							#1sn bekle                                          
-	GPIO.output(pin,GPIO.LOW)               #ledi sondur
+	GPIO.output(led1,GPIO.LOW)               #ledi sondur
 	time.sleep(1)							#1sn bekle 
